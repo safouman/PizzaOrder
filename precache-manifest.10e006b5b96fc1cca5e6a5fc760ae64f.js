ada50942ed0f935bff8b1c5ef70d82db08c1ffca
Safwen Daghsen <safouman@gmail.com>
2019-06-02 11:49:45 +0100

    Updates

---
+++ precache-manifest.10e006b5b96fc1cca5e6a5fc760ae64f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b7705161f15e5998ce",
+    "url": "/PizzaOrder/static/css/main.2b72526c.chunk.css"
+  },
+  {
+    "revision": "b8b7705161f15e5998ce",
+    "url": "/PizzaOrder/static/js/main.37a16e50.chunk.js"
+  },
+  {
+    "revision": "f73ea2cbb91e5348763f",
+    "url": "/PizzaOrder/static/js/runtime~main.ddfae4f8.js"
+  },
+  {
+    "revision": "6cbad67a5da557c5fed8",
+    "url": "/PizzaOrder/static/js/2.c381d7e0.chunk.js"
+  },
+  {
+    "revision": "b4815e6aa1da561470a2c58a6a2d4d2b",
+    "url": "/PizzaOrder/index.html"
+  }
+];
